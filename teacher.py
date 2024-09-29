a3220c42a3d3db3b39f1203ea663ca582fb11330
stingpie <stingpie52@gmail.com>
2024-09-29 09:17:22 -0400

    removed streamlit

--- teacher.py
+++ teacher.py
@@ -1,9 +1,9 @@
 import anthropic
-import streamlit as st
+#import streamlit as st
 #import speech_recognition as sr
-import st_utils
+#import st_utils
 
-st.title('Teacher')
+#st.title('Teacher')
 
 
 
@@ -12,15 +12,6 @@ try:
 except:
     client = anthropic.Anthropic(api_key=input("Please type out your anthropic API key."))
 
-
-use_tts = st.checkbox("Would you like the website read aloud?")
-use_stt = st.checkbox("Would you like to speak instead of write?")
-use_tts=True
-
-
-lesson = st_utils.read_input("What would you like to lean today? Please give me a single word topic.", value="place", use_stt = use_stt)
-
-history=[{"role": "user", "content": "I'd like to learn about "+lesson}]
 
 
 
@@ -35,34 +26,59 @@ def list_files_recursive(path='.'):
     		filelist.append(os.path.join(root,file))
     return filelist
 
-examples=""
-for file in list_files_recursive("lessons"):
-    if(lesson in file and not open(file,'r').read() in examples):
-        print(file)
-        examples += "\n```\n"+open(file,'r').read()+"\n```\n"
+def lesson():
+    #print(st.query_params)
+    #params = st.query_params
+    #print(params)
+    #lesson = params['lesson']
+#    examples = st.query_params.examples
 
-print(examples)
+    #use_tts = st.checkbox("Would you like the website read aloud?")
+    #use_stt = st.checkbox("Would you like to speak instead of write?")
+    use_tts=False
+    use_stt=False
 
+    #lesson = st_utils.read_input("What would you like to lean today? Please give me a single word topic.", value="place", use_stt = use_stt)
+    lesson = input("What would you like to learn about today? please type a one word topic.")
+    history=[{"role": "user", "content": "I'd like to learn about "+lesson}]
 
-
-for i in range(25):
     
-    with st.spinner('Generating response...'):
+
+    examples=""
+    for file in list_files_recursive("."):
+        if(lesson in file):
+            examples += "\n```\n"+open(file,'r').read()+"\n```\n"
+    if len(examples)==0:
+        examples += "\n```\n"+open("lessons/math/one_ten_hundred_place.txt",'r').read()+"\n```\n"
+
+
+    i=0 
+    while(1):
+        i+=1
+        #with st.spinner('Generating response...'):
         message = client.messages.create(
-            model="claude-3-5-sonnet-20240620",
-            max_tokens=1024,
-            system = open("prompt.txt",'r').read() + "For example, these are some lessons you've done in the past: "+examples,
-            messages=history
-        )
-    if("Student:" in message.content[0].text or "student:" in message.content[0].text):
-        
-        message.content[0].text = message.content[0].text[:message.content[0].text.lower().find("student:")]
+                model="claude-3-5-sonnet-20240620",
+                max_tokens=1024,
+                system = open("prompt.txt",'r').read() + "For example, these are some lessons you've done in the past: "+examples,
+                messages=history
+            )
+        if("Student:" in message.content[0].text or "student:" in message.content[0].text):
+            
+            message.content[0].text = message.content[0].text[:message.content[0].text.lower().find("student:")]
+        if "\n" in message.content[0].text:
+            message.content[0].text = (message.content[0].text.split("\n"))[0]
 
-    history += [{"role":"assistant","content":message.content[0].text}]
 
-    st_utils.output_text(message.content[0].text, use_tts = use_tts)
-    new_response = st_utils.read_input("Your response",key=23*i+123131244, use_stt=use_stt)
-    while(len(new_response)==0):
-        pass
-    history += [{"role":"user","content":new_response}]
+        history += [{"role":"assistant","content":("Teacher: "if(not message.content[0].text.lower().startswith("teacher:"))else "") +message.content[0].text}]
     
+        #st_utils.output_text(message.content[0].text, use_tts = use_tts)
+        print(message.content[0].text)
+        #new_response = st_utils.read_input("Your response",key=23*i+123131244, use_stt=use_stt)
+        new_response = "Student: " + input()
+        history += [{"role":"user","content":new_response}]
+
+
+
+
+if __name__=="__main__":
+    lesson()
